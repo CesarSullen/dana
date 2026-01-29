1f7f63cd561d7e8e9e3345938df3591ac62cc78a
saru <tools.customary833@passfwd.com>
2026-01-29 11:45:14 -0500

    added wave

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ navLinks.forEach((link) => {
 	});
 });
 
-// Accordion
+//	Accordion
 const faqItems = document.querySelectorAll(".faq-item");
 
 faqItems.forEach((item) => {
@@ -30,3 +30,22 @@ faqItems.forEach((item) => {
 		item.classList.toggle("active");
 	});
 });
+
+//	Interception Observer
+const animatedElements = document.querySelectorAll(
+	".show-up, .show-down, .show-left, .show-right, .bounce-in, .rotate-left, .rotate-right"
+);
+
+const observer = new IntersectionObserver(
+	(entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("animated");
+				observer.unobserve(entry.target);
+			}
+		});
+	},
+	{ root: null, rootMargin: "0px", threshold: 0.2 }
+);
+
+animatedElements.forEach((el) => observer.observe(el));
